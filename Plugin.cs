6e34d16147bdddfec0e04682e3c3f9e22e1e9781
Linit <109791089+Lcinit@users.noreply.github.com>
2024-05-01 23:41:09 +0800

    Update Plugin.cs
    ToggleMute soft dependency

--- Plugin.cs
+++ Plugin.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 namespace CursedScraps
 {
     [BepInPlugin(modGUID, modName, modVersion)]
+    [BepInDependency("quackandcheese.togglemute", BepInDependency.DependencyFlags.SoftDependency)]//ToggleMute soft dependency
     internal class CursedScraps : BaseUnityPlugin
     {
         private const string modGUID = "Lega.CursedScraps";
@@ -36,6 +37,7 @@ namespace CursedScraps
             harmony.PatchAll(typeof(ItemManagerPatch));
             harmony.PatchAll(typeof(PlayerManagerPatch));
             harmony.PatchAll(typeof(EnemyAIPatch));
+            PlayerManagerPatch.UDToggleMute(harmony);// Detects ToggleMute and disables it when MUTE
         }
     }
 }
